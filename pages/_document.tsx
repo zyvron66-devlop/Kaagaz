3400d641927f1bde17dbd79e81b37e2bcaa4dbc8
zyvron66-devop <325523186+zyvron66-devlop@users.noreply.github.com>
2026-09-15 19:40:09 +0530

    Add Google Search Console verification

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="absBMOi6umuUqsPdCfvalZlOyF8xrbLhIph748M8yvw"
+        />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
