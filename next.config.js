a8dfb2b9d16f653139e0b41a73540b460737f592
zyvron66-devop <325523186+zyvron66-devlop@users.noreply.github.com>
2026-09-15 19:03:22 +0530

    Configure GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+
+  output: "export",
+
+  basePath: "/Kaagaz",
+
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
